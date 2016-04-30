6dd284c708d0f670295f159920e9960530fe9b04
Maxim <fomaxi@gmail.com>
2016-04-30 21:49:18 +0300

    implement ArrayAccess, IteratorAggregate

--- Models/ArrayCollection.php
+++ Models/ArrayCollection.php
@@ -2,7 +2,29 @@
 
 namespace Models;
 
-class ArrayCollection extends Collection
+class ArrayCollection extends Collection implements \ArrayAccess, \IteratorAggregate
 {
+	public function offsetSet($offset, $value) {
+		if (is_null($offset)) {
+			$this->_items[] = $value;
+		} else {
+			$this->_items[$offset] = $value;
+		}
+	}
+
+	public function offsetExists($offset) {
+		return isset($this->_items[$offset]);
+	}
+
+	public function offsetUnset($offset) {
+		unset($this->_items[$offset]);
+	}
+
+	public function offsetGet($offset) {
+		return isset($this->_items[$offset]) ? $this->_items[$offset] : null;
+	}
 	
+	public function getIterator() {
+		return new \ArrayIterator($this->_items);
+	}
 }
--- Models/ArrayModel.php
+++ Models/ArrayModel.php
@@ -2,7 +2,25 @@
 
 namespace Models;
 
-class ArrayModel extends Model
+class ArrayModel extends Model implements \ArrayAccess
 {
-	
+	public function offsetSet($offset, $value) {
+		if (is_null($offset)) {
+			$this->_attributes[] = $value;
+		} else {
+			$this->_attributes[$offset] = $value;
+		}
+	}
+
+	public function offsetExists($offset) {
+		return isset($this->_attributes[$offset]);
+	}
+
+	public function offsetUnset($offset) {
+		unset($this->_attributes[$offset]);
+	}
+
+	public function offsetGet($offset) {
+		return isset($this->_attributes[$offset]) ? $this->_attributes[$offset] : null;
+	}
 }
